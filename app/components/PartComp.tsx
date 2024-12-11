230d07cda12c94fdcbab4dea8611839786baf7b9
Marina Safwat <66208355+marinaSwadee@users.noreply.github.com>
2024-12-11 12:20:20 +0200

    comment un needed content

--- app/components/PartComp.tsx
+++ app/components/PartComp.tsx
@@ -1,12 +1,12 @@
-import React from "react";
-import ParticlesComponent from "./particles";
+// import React from "react";
+// import ParticlesComponent from "./particles";
 
-const PartComp = () => {
-  return (
-    <div className="w-full h-[500px]">
-      <ParticlesComponent id="particles" />
-    </div>
-  );
-};
+// const PartComp = () => {
+//   return (
+//     <div className="w-full h-[500px]">
+//       <ParticlesComponent id="particles" />
+//     </div>
+//   );
+// };
 
-export default PartComp;
+// export default PartComp;
